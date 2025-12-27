d427a95122a027f542d736d7ec7fe69b7dd07a1b
jchaeyun <jocheyoon@gmail.com>
2025-12-27 12:43:28 +0100

    [level 2] Title: 성분으로 구분한 아이스크림 총 주문량, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
+++ 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
@@ -10,3 +10,10 @@ GROUP BY
     I.INGREDIENT_TYPE
 ORDER BY
     TOTAL_ORDER ASC
+    -- ORDER BY에서는 Alias(TOTAL_ORDER)를 쓰므로 F.를 안 붙여도 됨
+    
+/*
+-두 테이블에 같은 이름의 컬럼이 있을 때 
+별칭(I,F) 반드시 붙임. DB가 구분하지 못해 에러 발생 (예: FLAVOR)
+-한 테이블에만 있는 컬럼일 때
+붙이는 것을 추천. 나중에 같은 이름의 컬럼이 다른 테이블에 생길 경우를 대비 + 가독성*/
