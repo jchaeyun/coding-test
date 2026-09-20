7394f70a2573c2435a8697004b40aa0f1a05c789
jchaeyun <jocheyoon@gmail.com>
2026-09-20 15:42:59 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 4.92 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -5,31 +5,34 @@
 using namespace std;
 
 int solution(string name) {
-   //세로방향 이동부터 기록
+    //세로
     int cnt=0;
-    int n=name.size();
-    for(int i=0;i<n;i++){
-        if(name[i]<'N'){
-            cnt+=(name[i]-'A');
+    for(char& c:name){
+        if(c=='A') continue;
+        if(c>'A'&&c<='N') {
+            cnt+=(c-'A');
         }else{
-            cnt+=('Z'-name[i]+1);
-        } 
+            cnt+=('Z'-c+1);
+        }
+        
     }
     
-    //가로방향 이동->A인경우가 문제.
-    //오른쪽으로 가는거VS왔던길 되돌아간다음 마지막-타겟만큼이동
-    
-    int move=n-1;
-    
+    //가로
+    //1.기본값:왼쪽으로 쭉가기
+    //2.왼쪽으로 i만큼 갔다가 돌아가서 오른쪽으로
+    //3.오른쪽으로 갔다가 돌아가서 왼쪽으로
+    int move=name.size()-1;
+    int n=name.size();
     for(int i=0;i<n;i++){
         int next=i+1;
-        while(next<n&&name[next]=='A') next++;
-        move=min(move,i*2+(n-next)); //오른쪽으로 먼저 갔다가 돌아가서 왼쪽으로
-        move=min(move,(n-next)*2+i); //왼쪽으로 먼저 갔다가 돌아와서 오른쪽으로
+        while(next<n&&name[next]=='A'){
+            next++;
+        } 
+        
+            move=min(move,i*2+n-next);
+            move=min(move,(n-next)*2+i);
+        
     }
     
     return move+cnt;
-    
 }
-
-
