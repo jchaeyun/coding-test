f72e7989f0caaf043f75090ed8f27ff568c94eca
jchaeyun <jocheyoon@gmail.com>
2026-09-22 12:51:06 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 4.94 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -1,36 +1,31 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int solution(string name) {
-    //세로
     int cnt=0;
-    for(char& c:name){
-        if(c=='A') continue;
-        if(c>'A'&&c<='N') {
-            cnt+=(c-'A');
+    for(auto n:name){
+        if(n=='A') continue;
+        if(n<='N'){
+            cnt+=(n-'A');
         }else{
-            cnt+=('Z'-c+1);
+            cnt+=('Z'-n+1);
         }
         
     }
     
-    //가로
-    //1.기본값:왼쪽으로 쭉가기
-    //2.왼쪽으로 i만큼 갔다가 돌아가서 오른쪽으로
-    //3.오른쪽으로 갔다가 돌아가서 왼쪽으로
-    int move=name.size()-1;
     int n=name.size();
+    int move=n-1;//최대 이동
+    
     for(int i=0;i<n;i++){
         int next=i+1;
-        while(next<n&&name[next]=='A'){
+        while(name[next]=='A'){
             next++;
-        } 
+        }
         
-            move=min(move,i*2+n-next);
-            move=min(move,(n-next)*2+i);
+        move=min(move,i*2+n-next); //오른쪽으로 갔다 돌아와서 왼쪽으로
+        move=min(move,(n-next)*2+i); //왼쪽으로 갔다 돌아와서 오른쪽으로
         
     }
     
